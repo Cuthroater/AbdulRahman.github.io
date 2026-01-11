cac260cad24bb3e2f054d8e367425fcd3e2a4c47
Cuthroater <shalaby.abdo4@gmail.com>
2026-01-11 17:42:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/AbdulRahman.github.io/',  // Add this line
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
